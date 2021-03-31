bf707fcf32c0c2a4193bd65dfe381a1f7169203b
Перов Николай Борисович <rezzuer@gmail.com>
2021-04-01 00:30:43 +0300

    Work in progress

--- Task 1/Task 1/Cursor.h
+++ Task 1/Task 1/Cursor.h
@@ -1,5 +1,11 @@
 ﻿#pragma once
 
+enum class Orientation
+{
+	Horisontal,
+	Vertical
+};
+
 /**
  * \brief Класс курсор.
  */
@@ -10,14 +16,17 @@ public:
 	 * \brief Конструктор по умолчанию.
 	 */
 	Cursor();
+
 	/**
 	 * \brief Параметризованный конструктор.
 	 * \param x Координата X
 	 * \param y Координата Y
 	 * \param size Размер курсора
-	 * \param type Вид курсора
+	 * \param view Вид курсора
+	 * \param isVisible Видимость курсора
 	 */
-	Cursor(int x, int y, int size, bool type);
+	Cursor(int x, int y, int size, Orientation view, bool isVisible);
+
 	/**
 	 * \brief Деструктор
 	 */
@@ -28,61 +37,78 @@ public:
 	 * \return Координату X
 	 */
 	int getX() const;
+
 	/**
 	 * \brief Метод, возвращающий координату Y
 	 * \return Координату Y
 	 */
 	int getY() const;
+
 	/**
 	 * \brief Метод, возвращающий размер курсора
 	 * \return Размер курсора
 	 */
 	int getSize() const;
+
 	/**
 	 * \brief Метод, возвращающий вид курсора
 	 * \return Вид курсора
 	 */
-	bool getType() const;
+	Orientation getView() const;
+
 	/**
 	 * \brief Метод, изменяющий положение курсора по оси X
 	 * \param x Новое значение X
 	 */
 	void setX(int x);
+
 	/**
 	 * \brief Метод, изменяющий положение курсора по оси Y
 	 * \param x Новое значение Y
 	 */
 	void setY(int y);
+
 	/**
 	 * \brief Метод, изменяющий размер курсора
 	 * \param x Новое значение размера курсора
 	 */
 	void setSize(int size);
+
 	/**
 	 * \brief Метод, изменяющий вид курсора
 	 * \param x Вид курсора
 	 */
-	void setType(bool type);
+	void setView(Orientation view);
+
 	/**
 	 * \brief Метод, переключающий видимость курсора
+	 * \param isVisible Видимость курсора
 	 */
-	void toggleInvis();
+	void changeVisibility(bool isVisible);
 
 private:
 	/**
 	 * \brief Координата по оси X
 	 */
-	int X;
+	int x;
+
 	/**
 	 * \brief Координата по оси Y
 	 */
-	int Y;
+	int y;
+
 	/**
 	 * \brief Размер курсора
 	 */
-	int Size;
+	int size;
+
 	/**
 	 * \brief Вид курсора
 	 */
-	bool Type;
+	Orientation view;
+
+	/**
+	 * \brief Видимость курсора
+	 */
+	bool isVisible;
 };
